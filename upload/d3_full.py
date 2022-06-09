10bccd509bbdf3305628020e34b9e1f2c8cf0c0c
Lennart Kuell <lennart.kuell@gmail.com>
2022-06-09 17:52:20 +0200

    Fix duplicate authors

--- upload/d3_full.py
+++ upload/d3_full.py
@@ -10,10 +10,12 @@ import json
 import time
 import os
 from tqdm import tqdm
+import logging as log
 
 csv.field_size_limit(sys.maxsize)  # part 4 has a field that is too large
 token = open(".env").read()
 my_headers = {"Authorization": f"Bearer {token}", "Content-Type": "application/json"}
+log.basicConfig(format='%(message)s', stream=sys.stdout, level=log.INFO)
 
 
 def read_csv(filename):
@@ -36,9 +38,9 @@ def post(df_data, route, size=50):
         jdata = df_part.to_json(orient="records")
         res = requests.post(f"http://localhost:3000/api/v0/{route}", headers=my_headers, data=jdata)
         if res.status_code >= 400:
-            print(res)
-            print(res.json())
-            # print(df_part)
+            log.error(res)
+            log.error(res.json())
+            # log.error(df_part)
 
 
 def check_for_data(route):
@@ -47,14 +49,14 @@ def check_for_data(route):
 
 
 def list_venues(df_data, filename):
-    print("Create venues list")
+    log.info("Create venues list")
     if not os.path.isfile(filename):
         start = time.time()
         df_venues = pd.DataFrame({})
         df_venues["names"] = df_data["journal"].dropna().unique()
         df_venues.to_csv(filename)
         del df_venues
-        print(f"This took {(time.time() - start) / 60} min")
+        log.info(f"This took {(time.time() - start) / 60} min")
     gc.collect()
 
 
@@ -90,7 +92,7 @@ def list_authors(df_data, filename):
             fullname = " ".join(split[0: -1])
         return pd.Series([orcid, fullname, number])
 
-    print("Create authors list")
+    log.info("Create authors list")
     if not os.path.isfile(filename):
         start = time.time()
         df_authors = pd.DataFrame({})
@@ -98,18 +100,18 @@ def list_authors(df_data, filename):
         df_authors = df_authors.explode("author", ignore_index=True)  # list to single elements
         before = df_authors.shape[0]
         df_authors = df_authors.dropna()
-        print(f"Removed {before - df_authors.shape[0]} entries that were nan")
+        log.info(f"Removed {before - df_authors.shape[0]} entries that were nan")
         df_authors = df_authors["author"].apply(split_author)
-        print("Past split row")
+        log.info("Past split row")
         df_authors = df_authors.set_axis(["orcid", "fullname", "number"], axis=1)
         df_authors.to_csv(filename)
-        print(f"This took {(time.time() - start) / 60} min")
+        log.info(f"This took {(time.time() - start) / 60} min")
         del df_authors
     gc.collect()
 
 
 def combine_table(mode, table):
-    print(f"Combine {table} into single CSV")
+    log.info(f"Combine {table} into single CSV")
     filename = f"d3_{table}_{mode}.csv"
     if not os.path.isfile(filename):
         start = time.time()
@@ -118,21 +120,25 @@ def combine_table(mode, table):
             df_new = read_csv(f"d3_{table}_{mode}_{i}.csv")
             df_table = pd.concat([df_table, df_new])
         before = df_table.shape[0]
-        df_table = df_table.drop_duplicates()
-        print(f"Removed {before - df_table.shape[0]} duplicates")
+        if table == "authors":
+            df_table = df_table.sort_values(by=["orcid"], na_position="last")
+            df_table = df_table.drop_duplicates(subset=["fullname", "number"], keep="first")
+        else:
+            df_table = df_table.drop_duplicates()
+        log.info(f"Removed {before - df_table.shape[0]} duplicates")
         df_table.to_csv(filename)
-        print(f"This took {(time.time() - start) / 60} min")
+        log.info(f"This took {(time.time() - start) / 60} min")
 
 
 def post_table(table, mode, size=1000):
-    print(f"Post {table} to the backend")
+    log.info(f"Post {table} to the backend")
     if not check_for_data(table):
         start = time.time()
-        print(f"loading {table} from file")
+        log.info(f"Loading {table} from file")
         df_table = read_csv(f"d3_{table}_{mode}.csv")
-        print(f"sending {table} to backend")
+        log.info(f"Sending {table} to backend")
         post(df_table, table, size)
-        print(f"This took {(time.time() - start) / 60} min")
+        log.info(f"This took {(time.time() - start) / 60} min")
 
 
 def post_papers(mode):
@@ -140,18 +146,18 @@ def post_papers(mode):
     for i in range(1, 5):
         # if i == 4:
         start = time.time()
-        print(f"Post papers {i}/4 to the backend")
-        print(f"Loading papers {i}/4 from file")
+        log.info(f"Post papers {i}/4 to the backend")
+        log.info(f"Loading papers {i}/4 from file")
         df_papers = read_csv(f"d3_{mode}_edit_{i}.csv")
 
-        print(f"Converting fields with lists from string to list again")
+        log.info(f"Converting fields with lists from string to list again")
         df_papers["pdfUrls"] = df_papers["pdfUrls"].apply(lambda x: x if pd.isnull(x) else ast.literal_eval(x))
         df_papers["authors"] = df_papers["authors"].apply(ast.literal_eval)
         df_papers["fieldsOfStudy"] = df_papers["fieldsOfStudy"].apply(lambda x: [] if pd.isnull(x) else ast.literal_eval(x))
 
-        print(f"Sending papers {i}/4 to backend")
+        log.info(f"Sending papers {i}/4 to backend")
         post(df_papers, "papers", 10000)
-        print(f"This took {(time.time() - start) / 60} min")
+        log.info(f"This took {(time.time() - start) / 60} min")
         del df_papers
         gc.collect()
 
@@ -159,7 +165,7 @@ def post_papers(mode):
 def get_table(route, mode, size=1000):
     filename = f"d3_{route}_{mode}_ids.csv"
     if not os.path.isfile(filename):
-        print(f"Get {route} from the backend")
+        log.info(f"Get {route} from the backend")
         start = time.time()
         df = pd.DataFrame({})
         done = False
@@ -174,9 +180,9 @@ def get_table(route, mode, size=1000):
                 df = pd.concat([df, df_part])
         df = df.drop(columns=["createdAt", "createdBy", "__v"])
         df.to_csv(filename)
-        print(f"This took {(time.time() - start) / 60} min")
+        log.info(f"This took {(time.time() - start) / 60} min")
     else:
-        print(f"Load {route} from file")
+        log.info(f"Load {route} from file")
         df = read_csv(filename)
     return df
 
@@ -186,28 +192,23 @@ def map_venue_name_to_id(df):
     return dict(zip(df["names"], df["_id"]))
 
 
-def map_orcid_to_id(df):
-    df = df.dropna(subset=["orcid"])
-    return dict(zip(df["orcid"], df["_id"]))
-
-
 def map_author_name_to_id(df):
     df["name"] = df.apply(lambda row: row["fullname"] if pd.isnull(row["number"]) else f"{row['fullname']} {row['number']}", axis=1)
     return dict(zip(df["name"], df["_id"]))
 
 
-def transform_papers(df, filename, dict_venues, dict_authors, dict_orcid):
+def transform_papers(df, filename, dict_venues, dict_authors):
     def map_author(author):
         authors_raw = eval_author(author)
         authors = []
         for author in authors_raw:
             if isinstance(author, dict):
-                authors.append(dict_orcid.get(author["@orcid"]))
+                authors.append(dict_authors.get(author["#text"]))
             else:
                 authors.append(dict_authors.get(author))
         return authors
 
-    print("Drop and rename columns")
+    log.info("Drop and rename columns")
     # populated columns drop
     df = df.drop(columns=["@mdate", "pages", "authors", "volume", "number", "sources",
                           "journalVolume", "venue", "journalName", "journalPages", "doiUrl"])
@@ -220,34 +221,35 @@ def transform_papers(df, filename, dict_venues, dict_authors, dict_orcid):
     # columns maybe keep
     df = df.drop(columns=["month", "school"])
 
-    df = df.rename(columns={"id": "csvId", "paperAbstract": "abstractText", "journal": "venueRaw",
+    df = df.rename(columns={"id": "datasetId", "paperAbstract": "abstractText", "journal": "venueRaw",
                             "@key": "dblpId", "year": "yearPublished", "type": "typeOfPaper",
                             "inCitations": "inCitationsRef", "outCitations": "outCitationsRef",
                             "author": "authorsRaw"})
-    print("Set urls")
+    log.info("Set urls")
     # df["absUrl"] = "https://dblp.org/" + df["url"]
     # df = df.drop(columns=["url"])
 
-    print("Set citations")
+    log.info("Set citations")
     df["inCitationsCount"] = df["inCitationsRef"].apply(lambda x: 0 if pd.isnull(x) else len(ast.literal_eval(x)))
     df["outCitationsCount"] = df["outCitationsRef"].apply(lambda x: 0 if pd.isnull(x) else len(ast.literal_eval(x)))
 
     # TODO leave fields in
-    df = df.drop(columns=["inCitationsRef", "outCitationsRef", "csvId"])
+    df = df.drop(columns=["inCitationsRef", "outCitationsRef", "datasetId"])
     # df["inCitationsRef"] = df["inCitationsRef"].fillna({i: [] for i in df.index})  # empty List
     # df["outCitationsRef"] = df["outCitationsRef"].fillna({i: [] for i in df.index})  # empty List
 
-    print("Set access type")
+    log.info("Set access type")
     df["openAccess"] = df["ee"].str.contains("\"@type\": \"oa\"|@type: oa", na=False)
 
     df = df.drop(columns=["ee"])
 
-    print("Set venue")
+    log.info("Set venue")
     df["venue"] = df["venueRaw"].apply(dict_venues.get)
     df = df.drop(columns=["venueRaw"])
 
-    print("Set authors")
+    log.info("Set authors")
     df["authors"] = df["authorsRaw"].apply(map_author)
+        # for author in df.iloc[32]["authors"]:
     df = df.drop(columns=["authorsRaw"])
 
     df.to_csv(filename)
@@ -263,23 +265,23 @@ def analyse_cols(df_data: pd.DataFrame):
         if df_data[column].isnull().all():
             empty_cols.append(column)
         elif column in sparse_columns:
-            print(f"Unique values in {column}:\n{df_data[column].unique()}")
-    print(f"Empty columns: {empty_cols}")
+            log.info(f"Unique values in {column}:\n{df_data[column].unique()}")
+    log.info(f"Empty columns: {empty_cols}")
 
 
 def compare_cols(df_data: pd.DataFrame):
     df_data2 = df_data[["journal", "venue"]]
     subsetDataFrame2 = df_data2[df_data2['journal'] != df_data2["venue"]]
-    print(subsetDataFrame2)
+    log.info(subsetDataFrame2)
 
     df_data = df_data[["journal", "journalName"]]
     subsetDataFrame = df_data[df_data['journal'] != df_data["journalName"]]
-    print(subsetDataFrame)
+    log.info(subsetDataFrame)
 
 
 if __name__ == '__main__':
-    # mode = "test"
-    mode = "full"
+    mode = "test"
+    # mode = "full"
 
     # requirements: at least 32GB RAM; full dataset as CSV
     # note#1: steps below are needed for preparation and have to be executed in a shell
@@ -312,10 +314,10 @@ if __name__ == '__main__':
             df_part.to_csv(f"d3_test_{i}.csv")
 
     start = time.time()
-    print(mode + " mode")
+    log.info(mode + " mode")
     for i in range(1, 5):
         gc.collect()
-        print(f"Processing part {i}/4...")
+        log.info(f"Processing part {i}/4...")
 
         filename_venues = f"d3_venues_{mode}_{i}.csv"
         filename_authors = f"d3_authors_{mode}_{i}.csv"
@@ -323,13 +325,13 @@ if __name__ == '__main__':
             start_part = time.time()
             # analyse_cols(df_data)
             # compare_cols(df_data)
-            print(f"Load dataset {i}/4")
+            log.info(f"Load dataset {i}/4")
             df_data = load_dataset(f"d3_{mode}_{i}.csv")
             list_venues(df_data, filename_venues)
             list_authors(df_data, filename_authors)
             del df_data
 
-            print(f"Part {i}/4 took {(time.time()-start_part) / 60} min")
+            log.info(f"Part {i}/4 took {(time.time()-start_part) / 60} min")
         gc.collect()
 
     combine_table(mode, "venues")
@@ -338,9 +340,8 @@ if __name__ == '__main__':
     post_table("authors", mode, 1000)
     df_venues = get_table("venues", mode, size=10000)
     df_authors = get_table("authors", mode, size=10000)
-    print("Create dictionaries for id mappings")
+    log.info("Create dictionaries for id mappings")
     dict_venues = map_venue_name_to_id(df_venues)
-    dict_orcid = map_orcid_to_id(df_authors)
     dict_authors = map_author_name_to_id(df_authors)
     del df_venues
     del df_authors
@@ -348,19 +349,19 @@ if __name__ == '__main__':
 
     for i in range(1, 5):
         gc.collect()
-        print(f"Processing part {i}/4...")
+        log.info(f"Processing part {i}/4...")
         start_part = time.time()
 
         filename = f"d3_{mode}_edit_{i}.csv"
         if not os.path.isfile(filename):
-            print(f"Load dataset {i}/4")
+            log.info(f"Load dataset {i}/4")
             df_data = load_dataset(f"d3_{mode}_{i}.csv")
-            transform_papers(df_data, filename, dict_venues, dict_authors, dict_orcid)
+            transform_papers(df_data, filename, dict_venues, dict_authors)
             del df_data
 
-        print(f"Part {i}/4 took {(time.time() - start_part) / 60} min")
+        log.info(f"Part {i}/4 took {(time.time() - start_part) / 60} min")
         gc.collect()
 
     post_papers(mode)
 
-    print(f"Total time: {(time.time() - start)/60} min")
+    log.info(f"Total time: {(time.time() - start)/60} min")
